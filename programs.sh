abc2b29f1edfdb308b2efa01952a5a2fddf9b2c8
Arthur Moura Carvalho <armoucar@gmail.com>
2013-03-17 13:07:41 -0300

    appengine and android folder mappings

--- programs.sh
+++ programs.sh
@@ -9,8 +9,8 @@ export GREP_COLOR="4;33"
 export CLICOLOR="auto"
 export PS1='\w `~/.rvm/bin/rvm-prompt i v g` `git branch 2> /dev/null | grep -e ^* | sed -E  s/^\\\\\*\ \(.+\)$/\(\\\\\1\)\ /`\n\[\033[37m\]$\[\033[00m\] '
 
-export APPENGINE_SDK_HOME="~/desenvolvimento/appengine-java-sdk-1.7.5"
-export ANDROID_NDK="~/desenvolvimento/android-ndk-r8c"
+export APPENGINE_SDK_HOME="$HOME/desenvolvimento/appengine-java-sdk-1.7.5"
+export ANDROID_NDK="$HOME/desenvolvimento/android-ndk-r8c"
 export JAVA_HOME=`/usr/libexec/java_home -v 1.7`
 
 # MacPorts Installer addition
