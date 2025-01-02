adac0c0f010ad373d4c7a9e3884933c3ef4e5472
Arthur Moura Carvalho <armoucar@gmail.com>
2025-01-02 13:18:56 -0300

    --wip-- [skip ci]

--- dotfiles-pack.zsh
+++ dotfiles-pack.zsh
@@ -1,20 +1,24 @@
 #!/usr/bin/env zsh
 
-timestamp=$(date +%Y%m%d_%H%M%S)
-folder="tmp/$timestamp"
+function dotfiles-pack() {
+  timestamp=$(date +%Y%m%d_%H%M%S)
+  base_dir="$HOME/.oh-my-zsh/custom"
+  folder="$base_dir/tmp/$timestamp"
 
-mkdir -p tmp
-mkdir -p "$folder"
+  mkdir -p "$base_dir/tmp"
+  mkdir -p "$folder"
 
-if git archive --format=zip HEAD -o custom.zip &>/dev/null; then
-  if zip -q -r custom.zip .git &>/dev/null; then
-    mv custom.zip "$folder"/
-    echo "Successfully created and moved custom.zip to $folder"
+  cd "$base_dir"
+  if git archive --format=zip HEAD -o custom.zip &>/dev/null; then
+    if zip -q -r custom.zip .git &>/dev/null; then
+      mv custom.zip "$folder"/
+      echo "Successfully created and moved custom.zip to $folder"
+    else
+      echo "Error: Failed to add .git directory to zip file"
+      exit 1
+    fi
   else
-    echo "Error: Failed to add .git directory to zip file"
+    echo "Error: Failed to create git archive"
     exit 1
   fi
-else
-  echo "Error: Failed to create git archive"
-  exit 1
-fi
+}
