6c6a4eb5cab1c4f3333fa2799f6351c1268dc6ac
Arthur Moura Carvalho <armoucar@gmail.com>
2025-01-02 15:50:33 -0300

    --wip-- [skip ci]

--- init.zsh
+++ init.zsh
@@ -4,6 +4,10 @@ function init-dotfiles() {
   git clone \
     https://github.com/zsh-users/zsh-autosuggestions \
     ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions 2>/dev/null || {
-      echo "Note: zsh-autosuggestions plugin directory already exists, skipping clone..."
-    }
+    echo "Note: zsh-autosuggestions plugin directory already exists, skipping clone..."
+  }
+}
+
+function init-cli() {
+  pip install -e $HOME/.oh-my-zsh/custom/cli
 }
