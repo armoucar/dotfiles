ccc9cebdf8622ce1091bd002e7c83ca32866811b
Arthur Moura Carvalho <armoucar@gmail.com>
2025-01-26 12:24:06 -0300

    --wip-- [skip ci]

--- fzf.zsh
+++ fzf.zsh
@@ -1,2 +1,9 @@
-export FZF_DEFAULT_COMMAND='find . -type f -not -path "*/\.git/*" -not -path "*/node_modules/*" -not -path "*/vendor/*" -not -path "*/.build/*" -not -path "*/.swiftpm/*"'
+export FZF_DEFAULT_COMMAND='find . -type f \
+  -not -path "*/\.git/*" \
+  -not -path "*/.venv/*" \
+  -not -path "*/node_modules/*" \
+  -not -path "*/vendor/*" \
+  -not -path "*/.build/*" \
+  -not -path "*/.swiftpm/*"'
+
 export FZF_CTRL_T_COMMAND="$FZF_DEFAULT_COMMAND"
