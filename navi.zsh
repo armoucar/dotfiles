d23683b39630289e7fc4ea81ad699a471322af01
Arthur Moura Carvalho <armoucar@gmail.com>
2025-02-23 14:46:40 -0300

    --wip-- [skip ci]

--- navi.zsh
+++ navi.zsh
@@ -3,9 +3,3 @@
 export NAVI_PATH=$HOME/.oh-my-zsh/custom/navi
 
 eval "$(navi widget zsh)"
-
-export NEO_CLI_PATH=$HOME/dev/workspace/neon.neo-cli
-
-for dir in $NEO_CLI_PATH/neo/*/; do
-  export NAVI_PATH=$NAVI_PATH:$dir
-done
