5ba76390239f005d414a967c30cd1a2cd9760068
Arthur Moura Carvalho <armoucar@gmail.com>
2025-04-30 21:28:50 -0300

    --wip-- [skip ci]

--- gh.zsh
+++ gh.zsh
@@ -1,1 +1,6 @@
 export PAGER=""
+
+function gh-actions() {
+  open -a "Google Chrome" --args --profile-directory="Default"
+  open "https://github.com/$(gh repo view --json nameWithOwner -q .nameWithOwner)/actions"
+}
