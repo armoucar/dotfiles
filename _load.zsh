846878ec3c6b0a4294618f13dbcc2b82b9af11db
Arthur Moura Carvalho <armoucar@gmail.com>
2025-07-30 11:07:10 -0300

    --wip-- [skip ci]

--- _load.zsh
+++ _load.zsh
@@ -6,16 +6,16 @@ alias docker="podman"
 export PATH=$PATH:$HOME/.oh-my-zsh/custom/bin
 export PATH=$PATH:$HOME/.oh-my-zsh/custom/bin-public
 
+# Load private files
+for file in $HOME/.oh-my-zsh/custom/private/*.zsh; do
+  source "$file"
+done
+
+source $HOME/.oh-my-zsh/custom/cli/app/command/notes/alias.zsh
+
 # Load all zsh files from the new structure
 for category in core tools languages containers personal setup; do
   for file in $HOME/.oh-my-zsh/custom/zsh/$category/*.zsh(N); do
     source "$file"
   done
 done
-
-# Load private files
-for file in $HOME/.oh-my-zsh/custom/private/*.zsh; do
-  source "$file"
-done
-
-source ~/.oh-my-zsh/custom/cli/app/command/notes/alias.zsh
